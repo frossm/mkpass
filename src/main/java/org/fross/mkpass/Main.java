471387e7ea3d1d0efb9e8759aae35d5ae1cacaa3
Michael Fross <michael@fross.org>
2020-07-27 13:43:29 -0500

    Added MIT license information to the source file boilerplate

--- src/main/java/org/fross/mkpass/Main.java
+++ src/main/java/org/fross/mkpass/Main.java
@@ -1,13 +1,32 @@
-/*
+/******************************************************************************************
  * mkpass: Generate Secure Passwords on the command line
  * 
- * This code is directly from the Wikipedia article:
+ * This code is based on the following Wikipedia article:
  * https://en.wikipedia.org/wiki/Random_password_generator
  * 
  * I've made some usability enhancements however to make it easier to use for me.
  * 
- */
-
+ *  Copyright (c) 2020 Michael Fross
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * 
+ ******************************************************************************************/
 package org.fross.mkpass;
 
 import java.io.IOException;
