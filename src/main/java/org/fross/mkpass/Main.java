5f9dc307d3809c7070c33dacc40ce91b7443add8
Michael Fross <michael@fross.org>
2023-12-29 11:36:28 -0600

    Updated copyright date to include 2024 Also pulled in some updates for the GenChecksums.py file

--- src/main/java/org/fross/mkpass/Main.java
+++ src/main/java/org/fross/mkpass/Main.java
@@ -6,7 +6,7 @@
  * 
  * I've made some usability enhancements however to make it easier to use for me.
  * 
- *  Copyright (c) 2020-2022 Michael Fross
+ *  Copyright (c) 2020-2024 Michael Fross
  *
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
